4456f2fc28b8bac2299ac25852a3e626c8c73fb0
egilp04 <egilp04@iesalbarregas.es>
2025-11-12 12:05:41 +0100

    cambios en el modelo de contrato

--- src/app/models/Contrato.ts
+++ src/app/models/Contrato.ts
@@ -1,8 +1,11 @@
 export interface ContratoModel {
   id_contrato?: number;
-  id_usuario: number;
+  id_usuarioCliente: number;
+  id_usuarioEmpresa: number;
   id_servicio_horario: number;
   estado: 'activo' | 'no activo';
   fecha_inicio: string;
   fecha_fin: string;
+  dia_semana_copia: 'lunes' | 'martes' | 'miercoles' | 'jueves' | 'viernes';
+  hora_copia: string;
 }
